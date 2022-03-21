b412e7cc85b9f70f5419cc810d7c6a0157a6460a
Nazrul Islam <nazrul.islam@technovista.com.bd>
2022-03-21 18:35:47 +0600

    Algorithm

--- Section_02_Question_01/Section_02_Question_01/Program.cs
+++ Section_02_Question_01/Section_02_Question_01/Program.cs
@@ -8,5 +8,55 @@ namespace Section_02_Question_01
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void myFunction(int[] A, int L, int R)
+        {
+            int I, J, P, T;
+
+            for(int i=0; i<=A.Length; i++)
+            {
+                I = L; J = R;
+                //P:= A[(L + R) shr 1];
+                P = A[L + R - 1];
+                //while A[I] < P
+                while (A[I] < P)
+                {
+                    //Inc(I);
+                }
+
+                //while A[J] > P
+                while (A[J] < P)
+                {
+                    //Dec(J);
+                }
+
+                //if I <= J then
+                //begin
+                //T:= A[I];
+                //A[I] := A[J];
+                //A[J] := T;
+                //Inc(I);
+                //Dec(J);
+                //end;
+                //until I > J;
+                //if L < J then
+                //f(A, L, J);
+                //  L:= I;
+                //until I >= R;
+
+                if (I <= J)
+                {
+                    T = A[I];
+                    A[I] = A[J];
+                    A[J] = T;
+                }
+
+                if(L < J)
+                {
+                    myFunction(A, L, J);
+                    L = I;
+                }
+            }
+        }
     }
 }
