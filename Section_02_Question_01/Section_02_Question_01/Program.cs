765dd108db15ee7ad56ca6b05e29560969c31619
Nazrul Islam <nazrul.islam@technovista.com.bd>
2022-03-22 17:25:14 +0600

    update Sec_02_Q_01

--- Section_02_Question_01/Section_02_Question_01/Program.cs
+++ Section_02_Question_01/Section_02_Question_01/Program.cs
@@ -6,57 +6,62 @@ namespace Section_02_Question_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = {1,2,3,4};
+
+            myFunction(arr, 0, 2);
         }
 
         static void myFunction(int[] A, int L, int R)
         {
             int I, J, P, T;
-
-            for(int i=0; i<=A.Length; i++)
-            {
+            do{
                 I = L; J = R;
                 //P:= A[(L + R) shr 1];
-                P = A[L + R - 1];
-                //while A[I] < P
-                while (A[I] < P)
+                P = A[L + R + 1];
+                do
                 {
+                    //while A[I] < P
+                    while (A[I] < P)
+                    {
+                        //Inc(I);
+                        I = I + 1;
+                    }
+                    //while A[J] > P
+                    while (A[J] > P)
+                    {
+                        //Dec(J);
+                        J = J - 1;
+                    }
+                    //if I <= J then
+                    //begin
+                    //T:= A[I];
+                    //A[I] := A[J];
+                    //A[J] := T;
                     //Inc(I);
-                }
-
-                //while A[J] > P
-                while (A[J] < P)
-                {
                     //Dec(J);
-                }
+                    //end;
+                    //until I > J;
+                    //if L < J then
+                    //f(A, L, J);
+                    //  L:= I;
+                    //until I >= R;
 
-                //if I <= J then
-                //begin
-                //T:= A[I];
-                //A[I] := A[J];
-                //A[J] := T;
-                //Inc(I);
-                //Dec(J);
-                //end;
-                //until I > J;
-                //if L < J then
-                //f(A, L, J);
-                //  L:= I;
-                //until I >= R;
+                    if (I <= J)
+                    {
+                        T = A[I];
+                        A[I] = A[J];
+                        A[J] = T;
+                        I = I + 1;
+                        J = J - 1;
+                    }
+                } while (I <= J);
 
-                if (I <= J)
-                {
-                    T = A[I];
-                    A[I] = A[J];
-                    A[J] = T;
-                }
-
-                if(L < J)
+                if (L < J)
                 {
                     myFunction(A, L, J);
-                    L = I;
                 }
-            }
+                L = I;
+            }while (I < R) ;
         }
     }
 }
