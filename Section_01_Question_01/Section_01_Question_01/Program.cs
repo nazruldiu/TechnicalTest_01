d12ae5a9a2a41ae0c38b5a20ee1ea3c4257e00f1
Nazrul Islam <nazrul.islam@technovista.com.bd>
2022-03-22 19:00:52 +0600

    ...

--- Section_01_Question_01/Section_01_Question_01/Program.cs
+++ Section_01_Question_01/Section_01_Question_01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Section_01_Question_01
 {
@@ -6,7 +9,29 @@ namespace Section_01_Question_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Get("<p> Hello <b> Bangladesh </b> </p>");
+        }
+
+        static void Get(string file)
+        {
+            var input = file;
+            var token = "<>";
+
+            var allTags = input.Split().Where(str => token.All(c => str.Contains(c))).ToArray();
+
+            for (int i= 0; i < allTags.Length; i++)
+            {
+                Console.WriteLine(allTags[i]);
+                var currentTag = allTags[i];
+                int j = 0;
+                while(j < currentTag.Length)
+                {
+                    if(j+1 < currentTag.Length && currentTag[j] == '<' && currentTag[j+1] == '/')
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
