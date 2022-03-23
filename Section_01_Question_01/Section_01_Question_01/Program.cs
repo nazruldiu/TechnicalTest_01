fe958ec7317b393dfa569ec4db65d76e5e83b2a9
nazruldiu <nazruldiu26@gmail.com>
2022-03-24 01:17:35 +0600

    Final Touch

--- Section_01_Question_01/Section_01_Question_01/Program.cs
+++ Section_01_Question_01/Section_01_Question_01/Program.cs
@@ -9,29 +9,48 @@ namespace Section_01_Question_01
     {
         static void Main(string[] args)
         {
-            Get("<p> Hello <b> Bangladesh </b> </p>");
+            Get("<p>Hello <b> Bangladesh </b></p>");
         }
 
         static void Get(string file)
         {
             var input = file;
-            var token = "<>";
 
-            var allTags = input.Split().Where(str => token.All(c => str.Contains(c))).ToArray();
+            var pattern = @"(?<=\<)(.*?)(?=\>)";
+            var regex = new Regex(pattern);
+            var allTags = regex.Matches(file).ToArray();
 
-            for (int i= 0; i < allTags.Length; i++)
+            for (int i = 0; i < allTags.Length; i++)
             {
-                Console.WriteLine(allTags[i]);
-                var currentTag = allTags[i];
-                int j = 0;
-                while(j < currentTag.Length)
+                var currentTag = allTags[i].ToString();
+                if (currentTag[0] == '/')
                 {
-                    if(j+1 < currentTag.Length && currentTag[j] == '<' && currentTag[j+1] == '/')
-                    {
+                    Console.WriteLine("Expected # found {0}", "<" + currentTag + ">");
+                    return;
+                }
+                var currentClsTag = "/"+ currentTag;
+                bool isFound = false;
 
+                for (int j = i + 1; j < allTags.Length; j++)
+                {
+                    if (currentClsTag.ToString() == allTags[j].ToString())
+                    {
+                        isFound = true;
+                        int firstFoundIndex = Array.IndexOf(allTags, allTags[j]);
+                        allTags = allTags.Where((source, index) => index != firstFoundIndex).ToArray();
+
+                        break;
                     }
                 }
+
+                if (!isFound)
+                {
+                    Console.WriteLine("Expected {0} found #", "<" + currentClsTag.ToString() + ">");
+                    return;
+                }
             }
+
+            Console.WriteLine("Correctly tagged paragraph");
         }
     }
 }
